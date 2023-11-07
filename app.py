f4f8149622cc48fd632f455e9062d683c7ed074d
Mathew-B-SDGS <mathew.baines@nhs.net>
2023-11-07 16:01:42 +0000

    bug fixed implementation of sadie

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
-from modules import test_api
+from modules import test_api, PanelAppApi
+
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,6 +9,8 @@ def hello_world():
     return """<h1>This is the front end for out app so far</h1>
             <p>It's not much but it's a start</p>
             <a href="/test">Test data</a>
+            <a href="/allpanels">All Panels</a>
+            <a href="/test/1">Test Panel</a>
             """
 
 
@@ -14,6 +18,14 @@ def hello_world():
 def test_data():
     return test_api.TestApi.allpanels()
 
-@app.route("/test/<int:panel_id>")
-def test_panel(panel_id):
-    return test_api.TestApi.panel(panel_id)
+@app.route("/test/<r>")
+def test_panel(r):
+    # Create an instance of the ApiCallsSadie class
+    api_calls_sadie = PanelAppApi.ApiCallsSadie()
+    return api_calls_sadie.return_specific_panel(r)
+
+@app.route("/allpanels")
+def all_panels():
+    # Create an instance of the ApiCallsSadie class
+    api_calls_sadie = PanelAppApi.ApiCallsSadie()
+    return api_calls_sadie.get_panels_for_genomic_test()
