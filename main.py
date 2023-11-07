b46628fed83a8f361d7712043365406e39698098
Mathew-B-SDGS <mathew.baines@nhs.net>
2023-11-07 13:13:14 +0000

    added CLI functionality

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-print("hello Sadie and Huma")
-# Path: main.py 
+import click
+
+@click.command()
+@click.argument('rnumber')
+@click.option("--writefile", is_flag=True, help="Output to file")
+def query_api(rnumber, writefile):
+    """Access the Panel-APP Api."""
+    return_value= function(rnumber)
+
+    click.echo('Accessing the Panel-APP Api...')
+    click.echo(f'Rnumber: {rnumber}')
+    if writefile:
+        with open('output.txt', 'w') as f:
+            f.write(rnumber)
+
+
+
+    pass
+if __name__ == '__main__':
+    query_api()
+
