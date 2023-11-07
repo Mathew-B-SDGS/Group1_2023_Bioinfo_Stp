8debf9b771bf84ecb0ad06de0b92b5793dc51316
Mathew-B-SDGS <mathew.baines@nhs.net>
2023-11-07 22:31:37 +0000

    add excel parser and intergate with flask app

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, render_template
-from modules import test_api, PanelAppApi
+from modules import test_api, PanelAppApi, ParserExcel
 
 
 app = Flask(__name__)
+app.debug = True  
+
 
 @app.route("/")
 def hello_world():
@@ -12,18 +14,38 @@ def hello_world():
 @app.route("/search", methods=['POST'])
 def test_data():
     if request.method == 'POST':
-        r = request.form.get('r')
-        if r:
-            # Create an instance of the ApiCallsSadie class and fetch the panel
-            api_calls_sadie = PanelAppApi.ApiCallsSadie()
-            result = api_calls_sadie.return_specific_panel(r)
-            #check if the result has actually worked, Cant check for status code as its not a request module anymore
-            if result:
-                return result
+        r_number = request.form.get('r')
+        if r_number:
+            # Create an object instance of the ApiCallsSadie class and fetch the panel
+            api_calls_object = PanelAppApi.ApiCallsSadie()
+            filtered_api_result = api_calls_object.return_specific_panel(r_number)
+
+            # Create an object instance of the Parser class and parse the excel file
+            Parsed_results_object=ParserExcel.Parser()
+            filtered_df= Parsed_results_object.parse(r_number=r_number)
+
+            # Create a dictionary to pass to the results.html template for Jinja to render
+            jina_data= {"df":filtered_df, "r_json": filtered_api_result, "r":r_number}
+            
+            #if the api call has worked, render the results.html template with Jinja data
+            if filtered_api_result:
+                return render_template("results.html", results=jina_data)#
             else:
                 return "<p>Panel not found </p><a href='/'>Go back</a>"
         return "<p>No data provided or invalid request</p>"  # Handle cases where 'r' is not provided or other issues
     return "<p>GET request is not supported. Use the search form.</P."  # Inform users that a GET request is not supported
+
+@app.route("/testapi", endpoint="testapi")
+def test_data():
+    # api_object_testapi =  ()
+    return test_api.TestApi.allpanels()
+
+@app.route("/allpanels", endpoint="allpanels")
+def all_panels():
+    # Create an instance of the ApiCallsSadie class
+    api_calls_sadie = PanelAppApi.ApiCallsSadie()
+    return api_calls_sadie.get_panels_for_genomic_test()
+
 
 if __name__ == "__main__":
     app.run(debug=True)
@@ -32,21 +54,7 @@ if __name__ == "__main__":
 
 
 
-# @app.route("/test")
-# def test_data():
-#     return test_api.TestApi.allpanels()
 
-# @app.route("/test/<r>")
-# def test_panel(r):
-#     # Create an instance of the ApiCallsSadie class
-#     api_calls_sadie = PanelAppApi.ApiCallsSadie()
-#     return api_calls_sadie.return_specific_panel(r)
-
-# @app.route("/allpanels")
-# def all_panels():
-#     # Create an instance of the ApiCallsSadie class
-#     api_calls_sadie = PanelAppApi.ApiCallsSadie()
-#     return api_calls_sadie.get_panels_for_genomic_test()
 
 
 # @app.route("/test", methods=['GET', 'POST'])
@@ -57,3 +65,19 @@ if __name__ == "__main__":
 #             # Create an instance of the ApiCallsSadie class and fetch the panel
 #             api_calls_sadie = PanelAppApi.ApiCallsSadie()
 #         return api_calls_sadie.return_specific_panel(r)
+
+# @app.route("/search", methods=['POST'])
+# def test_data():
+#     if request.method == 'POST':
+#         r = request.form.get('r')
+#         if r:
+#             # Create an instance of the ApiCallsSadie class and fetch the panel
+#             api_calls_sadie = PanelAppApi.ApiCallsSadie()
+#             result = api_calls_sadie.return_specific_panel(r)
+#             #check if the result has actually worked, Cant check for status code as its not a request module anymore
+#             if result:
+#                 return result
+#             else:
+#                 return "<p>Panel not found </p><a href='/'>Go back</a>"
+#         return "<p>No data provided or invalid request</p>"  # Handle cases where 'r' is not provided or other issues
+#     return "<p>GET request is not supported. Use the search form.</P."  # Inform users that a GET request is not supported
