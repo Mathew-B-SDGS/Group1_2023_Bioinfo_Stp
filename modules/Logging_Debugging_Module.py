6fc543ee178f870b0077bdd27a24b51aca81c789
Huma <huma.zahid@mft.nhs.uk>
2023-11-09 14:12:50 +0000

    Completing the logging script and attempted the log file

--- modules/Logging_Debugging_Module.py
+++ modules/Logging_Debugging_Module.py
@@ -7,14 +7,10 @@ logger = logging.getLogger("mylogger")
 logger.setLevel(logging.DEBUG)
 
 # To log messages to a file 
-file_handler = logging.FileHandler
+file_handler = logging.FileHandler('mylogfile.log')
 
 # This directs the log messages to the output
 output_handler = logging.StreamHandler()
-
-# Formatting for the handlers 
-file_handler.setFormatter(formatter)
-output_handler.setFormatter(formatter)
 
 # There will be 2 handlers, one to write log messages to a file and the other for printing the messages to the output
 logger.addHandler(file_handler)
@@ -24,15 +20,14 @@ logger.addHandler(output_handler)
 def some_function():
     try: 
         result = perform_some_function()
-except Exception as e:
-# Log an error message if an exception occurs
-logger.error ("An error has occurred")
+    except Exception as e:
+        logger.error(f"Exception: {e}")
 
-if __name__ == "__main__": 
+# Log an error message if an exception occurs, this will determine the level of detail captured in the logs
     logger.debug("Start of application")
     some_function()
     logger.debug ("Exiting the application")
-    logger.info("Welcome Message")
+    logger.info("Info Message")
     logger.warning("Warning error message")
     logger.error("This is an Erorr message")
     logger.critical("Critical Message")
