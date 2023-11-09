8d3af335a4ccce852753497821e0ca9f454ca561
Mathew-B-SDGS <mathew.baines@nhs.net>
2023-11-09 17:05:31 +0000

    moved deprecated unused folder

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template, session
-from modules import test_api, PanelAppApi, ParserExcel, HGNC_converter , api_query_R
+from modules import ParserExcel, HGNC_converter , api_query_R
 
 app = Flask(__name__)
 app.debug = True
@@ -39,18 +39,6 @@ def test_data():
     return "<p>GET request is not supported. Use the search form.</P."
 
 
-@app.route("/testapi", endpoint="testapi")
-def test_data():
-    # api_object_testapi =  ()
-    return test_api.TestApi.allpanels()
-
-
-@app.route("/allpanels", endpoint="allpanels")
-def all_panels():
-    # Create an instance of the ApiCallsSadie class
-    api_calls_sadie = PanelAppApi.ApiCallsSadie()
-    return api_calls_sadie.get_panels_for_genomic_test()
-
 @app.route("/search/genelist",methods=['GET'], endpoint="genelist")
 def gene_list():
     r_number = session.get('r')
@@ -58,15 +46,6 @@ def gene_list():
         api_calls_object = api_query_R.ApiCallsbyR(r_number)
         filtered_api_result = api_calls_object.extract_genes_hgnc()
         return filtered_api_result
-        # list_of_genes = []
-        # if filtered_api_result:
-        #     for i in filtered_api_result:
-        #         HGNC_converter_object = HGNC_converter.HgncConverter(i)
-        #         json_object = HGNC_converter_object.ensembl_id_api_query(full_transcript_list=False)
-        #         list_of_genes.append(HGNC_converter_object)
-        #         del HGNC_converter_object, json_object
-        #     return list_of_genes
-        # return render_template("genelist.html", r=r_number, genes=list_of_genes)
     else:
         return "<p>No 'r' parameter found in the session</p>"
 
@@ -98,3 +77,15 @@ if __name__ == "__main__":
 #                 return "<p>Panel not found </p><a href='/'>Go back</a>"
 #         return "<p>No data provided or invalid request</p>"  # Handle cases where 'r' is not provided or other issues
 #     return "<p>GET request is not supported. Use the search form.</P."  # Inform users that a GET request is not supported
+
+# @app.route("/testapi", endpoint="testapi")
+# def test_data():
+#     # api_object_testapi =  ()
+#     return test_api.TestApi.allpanels()
+
+
+# @app.route("/allpanels", endpoint="allpanels")
+# def all_panels():
+#     # Create an instance of the ApiCallsSadie class
+#     api_calls_sadie = PanelAppApi.ApiCallsSadie()
+#     return api_calls_sadie.get_panels_for_genomic_test()
