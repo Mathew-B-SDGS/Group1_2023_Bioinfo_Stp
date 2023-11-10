80803bdde05642ba2e1759a85d4c4c21d0abbe43
Huma <huma.zahid@mft.nhs.uk>
2023-11-10 10:48:54 +0000

    Attempt to retrive refseq transcripts from variant validator

--- VariantValidatorapi.py
+++ VariantValidatorapi.py
@@ -2,14 +2,22 @@ import requests
 
 base_url = "https://rest.variantvalidator.org/VariantValidator/tools/gene2transcripts_v2/HGNC%3A4982/mane_select/refseq/GRCh37?content-type=application%2Fjson"
 
+class VariantValidatorRefseq:
+    def get_refseq_transcripts(self):
+# HTTP GET request to the API, using gene_id as a parameter
+        endpoint = f"{base_url}"
+        response = requests.get(endpoint)
+ # If request successful, json will return data on ref seq transcripts
+        if response.status_code == 200:
+           return response.json()
+        else:
+         print("Failed to retrieve refseq transcripts")
+         return None
 
-#def get_transcripts_from_gene_id(gene_id):
-   # try:
-        # HTTP GET request to the API, using gene_id as a parameter
+def return_mane_select_transcripts(self, HGVS):
+    refseq_transcripts = self.get_refseq_transcripts()
+    
 
-response = requests.get(base_url)
-        # If request successful, json will return data on ref seq transcripts
-refseq_transcripts = response.json()
-print (refseq_transcripts)
+
 
 
