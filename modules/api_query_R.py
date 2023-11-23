e17d02cb41aca2fd07f4f1f68925e9722f8c5fb9
ashs <sadie.ash@gosh.nhs.uk>
2023-11-23 15:09:38 +0000

    Create bed file with mulitiple genes

--- modules/api_query_R.py
+++ modules/api_query_R.py
@@ -6,7 +6,7 @@ class RcodeToBed():
     Run code by calling class with R test code and desired reference genome
     """
 
-    def __init__(self, test_code, ref_genome='GRCh37', padded_exons = True):
+    def __init__(self, test_code, ref_genome='GRCh38', padded_exons = True):
         self.test_code = test_code
         self.ref_genome = ref_genome
         self.padded_exons = padded_exons
@@ -34,29 +34,33 @@ class RcodeToBed():
     
     def get_coord_mane_select_trans(self):
         gene_list = self.extract_genes_hgnc()
+        print(gene_list)
         chrom_start_end = []
-        list_of_coords_for_bed = []
+        all_gene_coords = []
         for gene in gene_list:
-            vv_url = f"https://rest.variantvalidator.org/VariantValidator/tools/gene2transcripts_v2/{gene}/mane/ensembl/{self.ref_genome}"
-            print(vv_url)
+            vv_url = f"https://rest.variantvalidator.org/VariantValidator/tools/gene2transcripts_v2/{gene}/mane/refseq/{self.ref_genome}"
             response = requests.get(vv_url)
             if response.status_code == 200:
                 gene_info = response.json()
                 mane_select = (list(gene_info[0]['transcripts'][0]['genomic_spans'].keys()))[0]
                 transcript_data = gene_info[0]['transcripts'][0]['genomic_spans'][mane_select]
+                transcript_coords = []
                 if self.padded_exons:
                     for exon in transcript_data['exon_structure']:
                         chrom_start_end.append(gene_info[0]['transcripts'][0]['annotations']['chromosome'])
                         chrom_start_end.append(exon['genomic_start'])
                         chrom_start_end.append(exon['genomic_end'])
-                        list_of_coords_for_bed.append(chrom_start_end)
+                        transcript_coords.append(chrom_start_end)
                         chrom_start_end = []
                 else:
                     chrom_start_end.append(gene_info[0]['transcripts'][0]['annotations']['chromosome'])
                     chrom_start_end.append(transcript_data['start_position'])
                     chrom_start_end.append(transcript_data['end_position'])
-                    list_of_coords_for_bed.append(chrom_start_end)
-            return list_of_coords_for_bed
+                    transcript_coords.append(chrom_start_end)
+            all_gene_coords.append(transcript_coords)
+            transcript_coords = []
+        return all_gene_coords
+    
         
     def create_bed_file_iterable(self):
         # Specify the filename for the BED file
