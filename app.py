7ec52da7b91413accff2f21a055c17a073ba8762
Mathew-B-SDGS <mathew.baines@nhs.net>
2023-11-27 11:54:25 +0000

    created additonal blueprints

--- app.py
+++ app.py
@@ -1,119 +1,120 @@
-from flask import Flask, request, render_template, session , Response
-from modules import ParserExcel, bedmake
-from modules.deprecated import depreciated_HGNC_converter
+from flask import Flask, request, render_template, session, Response
+import os
 
-app = Flask(__name__)
-app.debug = True
-app.secret_key = "GoldSilverMoonStreamWoodpecker"
+from modules import ParserExcel, bedmake  # importing local modules ./modules/
+# importing local blueprints ./AppBlueprints/
+from AppBlueprints import database_blueprint, user_auth
 
-@app.route("/")
-def hello_world():
-    return render_template("home.html")
+# This is the main file that runs the app, it is the file that is run when you run the command 'flask run'
+# Below is the Factory function that creates the app, containing the app routes and blueprints
 
 
-@app.route("/search", methods=['POST'])
-def test_data():
-    if request.method == 'POST':
-        r_code = request.form.get('r')
-        session['r'] = r_code
-        if r_code:
-            # Create an object instance of the ApiCallsSadie class and fetch the panel
+def create_app(test_config=None):
+    """ 
+    IMPORTANT MAKE SURE TO INDENT ALL CODE IN THIS FUNCTION TO BE ALLIGNED
+
+    This is a factory function that creates the app, changed to this as it
+    is more flexible an allows us to change things in the config file without 
+    having to change the code in this file. ]
+    its also better if we want to deploy it anywhere else
+    """
+
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='GoldSilverMoonStreamWoodpecker',
+        # DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'), #ive commented this out as we are not using a database YET
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    """
+    all app routes are to be found below this comment
+    including 
+    - the home page
+    - the search page
+    - the gene list page
+    - the download page
+    
+    """
+
+    @app.route("/")
+    def hello_world():
+        return render_template("home.html")
+
+    @app.route("/search", methods=['POST'])
+    def test_data():
+        if request.method == 'POST':
+            r_code = request.form.get('r')
+            session['r'] = r_code
+            if r_code:
+                # Create an object instance of the ApiCallsSadie class and fetch the panel
+                api_calls_object = bedmake.RCodeToBedFile(r_code)
+                filtered_api_result = api_calls_object.get_panel_for_genomic_test()
+
+                # Create an object instance of the Parser class and parse the excel file
+                Parsed_results_object = ParserExcel.Parser()
+                filtered_df = Parsed_results_object.parse(r_code=r_code)
+
+                # Create a dictionary to pass to the results.html template for Jinja to render
+                jina_data = {"df": filtered_df,
+                             "r_json": filtered_api_result, "r": r_code}
+
+                # if the api call has worked, render the results.html template with Jinja data
+                if filtered_api_result:
+                    return render_template("results.html", results=jina_data)
+                else:
+                    return "<p>Panel not found </p><a href='/'>Go back</a>"
+            # Handle cases where 'r' is not provided or other issues
+            return "<p>No data provided or invalid request</p>"
+        # Inform users that a GET request is not supported
+        return "<p>GET request is not supported. Use the search form.</P."
+
+    @app.route("/search/genelist", methods=['GET'], endpoint="genelist")
+    def gene_list():
+        r_code = session.get('r')
+        if r_code is not None:
             api_calls_object = bedmake.RCodeToBedFile(r_code)
-            filtered_api_result = api_calls_object.get_panel_for_genomic_test()
+            filtered_api_result = api_calls_object.extract_genes_hgnc()
+            return filtered_api_result
+        else:
+            return "<p>No 'r' parameter found in the session</p>"
 
-            # Create an object instance of the Parser class and parse the excel file
-            Parsed_results_object = ParserExcel.Parser()
-            filtered_df = Parsed_results_object.parse(r_code=r_code)
+    @app.route("/search/download", endpoint="download", methods=['POST'])
+    def download_file():
+        r_code = session.get('r')
+        selected_action = request.form['action']
+        selected_lenght = request.form['version']
+        obj_for_bed = bedmake.RCodeToBedFile(r_code, padded_exons)
+        if selected_lenght == '150':
+            file_content = obj_for_bed.create_bed_file_iterable_150()
+        else:
+            file_content = obj_for_bed.create_bed_file_iterable()
+        response = Response(file_content, content_type='text/plain')
+        file_name = f'generated_file_{r_code}.bed'
+        response.headers['Content-Disposition'] = f'attachment; filename={file_name}'
+        return response
 
-            # Create a dictionary to pass to the results.html template for Jinja to render
-            jina_data = {"df": filtered_df,
-                         "r_json": filtered_api_result, "r": r_code}
+    """
+    all blueprints are to be found below this comment
+    (blueprints are modules of flask code that can be used to extend the app)
+    including
+    - the database blueprint
+    - the Auth blueprint (not yet implemented)
+    """
 
-            # if the api call has worked, render the results.html template with Jinja data
-            if filtered_api_result:
-                return render_template("results.html", results=jina_data)
-            else:
-                return "<p>Panel not found </p><a href='/'>Go back</a>"
-        # Handle cases where 'r' is not provided or other issues
-        return "<p>No data provided or invalid request</p>"
-    # Inform users that a GET request is not supported
-    return "<p>GET request is not supported. Use the search form.</P."
-
-
-@app.route("/search/genelist",methods=['GET'], endpoint="genelist")
-def gene_list():
-    r_code = session.get('r')
-    if r_code is not None:
-        api_calls_object = bedmake.RCodeToBedFile(r_code)
-        filtered_api_result = api_calls_object.extract_genes_hgnc()
-        return filtered_api_result
-    else:
-        return "<p>No 'r' parameter found in the session</p>"
-
-@app.route("/search/download", endpoint="download", methods=['POST'])
-def download_file():
-    r_code = session.get('r')
-    selected_action = request.form['action']
-    selected_lenght = request.form['version']
-    obj_for_bed = bedmake.RCodeToBedFile(r_code, padded_exons)
-    if selected_lenght == '150':
-        file_content = obj_for_bed.create_bed_file_iterable_150()
-    else:
-        file_content = obj_for_bed.create_bed_file_iterable()
-    response = Response(file_content, content_type='text/plain');
-    file_name = f'generated_file_{r_code}.bed'
-    response.headers['Content-Disposition'] = f'attachment; filename={file_name}'
-    return response
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
-
-# @app.route("/search/download150", endpoint="download150", methods=['POST'])
-# def download_file():
-#     selected_action = request.form['action']
-#     r_code = session.get('r')
-#     obj_for_bed = api_query_R.RCodeToBedFile(r_code, selected_action)
-#     file_content = obj_for_bed.create_bed_file_iterable_150()
-#     response = Response(file_content, content_type='text/plain');
-#     file_name = f'generated_file_{r_code}.bed'
-#     response.headers['Content-Disposition'] = f'attachment; filename={file_name}'
-#     return response
-
-# @app.route("/test", methods=['GET', 'POST'])
-# def test_data():
-#     if request.method == 'POST':
-#         r = request.form.get('r')
-#         if r:
-#             # Create an instance of the ApiCallsSadie class and fetch the panel
-#             api_calls_sadie = PanelAppApi.ApiCallsSadie()
-#         return api_calls_sadie.return_specific_panel(r)
-
-# @app.route("/search", methods=['POST'])
-# def test_data():
-#     if request.method == 'POST':
-#         r = request.form.get('r')
-#         if r:
-#             # Create an instance of the ApiCallsSadie class and fetch the panel
-#             api_calls_sadie = PanelAppApi.ApiCallsSadie()
-#             result = api_calls_sadie.return_specific_panel(r)
-#             #check if the result has actually worked, Cant check for status code as its not a request module anymore
-#             if result:
-#                 return result
-#             else:
-#                 return "<p>Panel not found </p><a href='/'>Go back</a>"
-#         return "<p>No data provided or invalid request</p>"  # Handle cases where 'r' is not provided or other issues
-#     return "<p>GET request is not supported. Use the search form.</P."  # Inform users that a GET request is not supported
-
-# @app.route("/testapi", endpoint="testapi")
-# def test_data():
-#     # api_object_testapi =  ()
-#     return test_api.TestApi.allpanels()
-
-
-# @app.route("/allpanels", endpoint="allpanels")
-# def all_panels():
-#     # Create an instance of the ApiCallsSadie class
-#     api_calls_sadie = PanelAppApi.ApiCallsSadie()
-#     return api_calls_sadie.get_panels_for_genomic_test()
+    # install the database blueprint into the module
+    app.register_blueprint(database_blueprint.bp, url_prefix='/database')
+    app.register_blueprint(user_auth.bp, url_prefix='/auth')
+    return app
