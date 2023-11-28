d119108f0b38a8e51497b7cc5aa87818b118389a
ashs <sadie.ash@gosh.nhs.uk>
2023-11-28 16:18:38 +0000

    Try to test bedmake

--- tests/api_query_test.py
+++ tests/api_query_test.py
@@ -1,5 +1,20 @@
 import pytest
+from modules.bedmake import RCodeToBedFile
 
-class TestApiQuery():
+
+class TestRCodetoBedFile:
+
+    def setUp(self):
+        self.test_r_code = 'R201'
+        self.ref_genome = 'GRCh38'
+        self.include_exon = 'Transcript'
+        self.clas = RCodeToBedFile(test_code=self.test_r_code, ref_genome=self.ref_genome,
+                                       include_exon=self.include_exon, padded=False)
     
-    def test_get_all_panels
+    def test_get_panel_for_genomic_test(self):
+        panel_app_response = 
+        panel_info = panel_app_response.get_panel_for_genomic_test()
+        panel_name = panel_info['name']
+        assert panel_name == 'Atypical haemolytic uraemic syndrome'
+
+        
