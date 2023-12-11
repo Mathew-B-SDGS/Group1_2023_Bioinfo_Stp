31c166ee613e9fc76c7670985ef69d8e5cfbde1f
Mathew-B-SDGS <mathew.baines@nhs.net>
2023-12-11 15:13:13 +0000

    working on uncerstain changes

--- AppBlueprints/database_blueprint.py
+++ AppBlueprints/database_blueprint.py
@@ -1,15 +1,60 @@
-from flask import Blueprint
+from flask import Blueprint, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
 
-bp = Blueprint('database', __name__, url_prefix='/database')
+blueprint_db = Blueprint('database', __name__, url_prefix='/database')
 
 
-@bp.route('/')
+class Base(DeclarativeBase):
+    '''base class for all tables'''
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+
+
+class Sample(db.Model):
+    """table holds Batch details, for all samples processed in a run"""
+    __tablename__ = "sample"
+
+    sample_id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, autoincrement=True, unique=True, nullable=False)
+    sample_name: Mapped[str] = mapped_column(String(30), nullable=False)
+    sample_type: Mapped[str] = mapped_column(String(30), nullable=True)
+
+
+def create_tables(context, db):
+    """create all tables"""
+    with context:
+        db.create_all()
+
+# another way to create the tables
+# Base.metadata.create_all(blueprint_db.engine)
+
+
+@blueprint_db.route('/')
 def hello():
     return "<h1>Database</h1><br><p>this module is for accessing the Database</p>"
 
 
-@bp.route('/users')
-def index():
-    users = User.query.all()
-    return render_template('index.html', users=users)
+@blueprint_db.route("/samples")
+def sample_list():
+    sample = db.session.execute(
+        db.select(Sample).order_by(Sample.sample_id)).scalars()
+    return sample
+    # return render_template("sample_list.html", sample=sample)
+
+
+@blueprint_db.route("/samples/create", methods=["GET", "POST"])
+def sample_create():
+    if request.method == "POST":
+        sample = Sample(
+            sample_name=request.form["sample_name"],
+            sample_type=request.form["sample_type"]
+        )
+        db.session.add(sample)
+        db.session.commit()
+        return redirect(url_for("database.sample_list"))
+    return "hello"
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, render_template, session, Response
+from flask import Flask, request, render_template, session, Response, Blueprint
 import os
-
+from AppBlueprints import database_blueprint
 from modules import ParserExcel, bedmake  # importing local modules ./modules/
 # importing local blueprints ./AppBlueprints/
 from AppBlueprints import database_blueprint, user_auth
@@ -25,6 +25,7 @@ def create_app(test_config=None):
         SECRET_KEY='GoldSilverMoonStreamWoodpecker',
         # DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'), #ive commented this out as we are not using a database YET
     )
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -40,110 +41,6 @@ def create_app(test_config=None):
         pass
 
     """
-    all app routes are to be found below this comment
-    including 
-    - the home page
-    - the search page
-    - the gene list page
-    - the download page
-    
-    """
-
-    @app.route("/")
-    def hello_world():
-        """
-        This is the home page route, it renders the home.html template with 
-        links to search an R number and to go to the database or Auth pages 
-        """
-        return render_template("home.html")
-
-    @app.route("/search", methods=['POST'])
-    def test_data():
-        """
-        This is the search page route, it renders the search.html template with
-        the search form and a link to the home page using the Data
-        """
-        if request.method == 'POST':
-            r_code = request.form.get('r')
-            session['r'] = r_code.upper()
-            if r_code:
-                # Create an object instance of the ApiCallsSadie class and fetch the panel
-                api_calls_object = bedmake.RCodeToBedFile(r_code, ref_genome='GRCh38')
-                filtered_api_result = api_calls_object.get_panel_for_genomic_test()
-                panel_name = filtered_api_result['name']
-                panel_version = filtered_api_result['version']
-
-                # Create an object instance of the Parser class and parse the excel file
-                Parsed_results_object = ParserExcel.Parser()
-                filtered_df = Parsed_results_object.parse(r_code=r_code)
-
-                # Create a dictionary to pass to the results.html template for Jinja to render
-                jijna_data = {"df": filtered_df,
-                             "r_json": filtered_api_result, "r": r_code.upper(),
-                             "panel_label": panel_name,
-                               "panel_version": panel_version}
-
-                # if the api call has worked, render the results.html template with Jinja data
-                if filtered_api_result:
-                    return render_template("results.html", results=jijna_data)
-                else:
-                    return "<p>Panel not found </p><a href='/'>Go back</a>"
-            # Handle cases where 'r' is not provided or other issues
-            return "<p>No data provided or invalid request</p>"
-        # Inform users that a GET request is not supported
-        return "<p>GET request is not supported. Use the search form.</P."
-
-    @app.route("/search/genelist", methods=['GET'], endpoint="genelist")
-    def gene_list():
-        r_code = session.get('r')
-        if r_code is not None:
-            api_calls_object = bedmake.RCodeToBedFile(r_code, ref_genome='GRCh38')
-            panel_info = api_calls_object.get_panel_for_genomic_test()
-            gene_name_panel = api_calls_object.extract_genes_hgnc()
-            panel_name = panel_info['name']
-            panel_version = panel_info['version']
-            data_genepage = {"gene_list": gene_name_panel,
-                             "panel_name": panel_name,
-                             "panel_version": panel_version}
-            return render_template("genelist.html", results=data_genepage)
-        else:
-            return "No 'r' parameter found in the session"
-
-    
-
-    @app.route("/search/download", endpoint="download", methods=['POST'])
-    def download_file():
-        r_code = session.get('r')
-        # choose between GRCh38 or GRCh37
-        selected_build = request.form['build']
-        # choose between Mane Select transcript coordinates or exon coordinates
-        exon_or_transcript = request.form['version']
-        # add an additonal 50bp padding to the exons or transcript
-        selected_padding = request.form['padding']
-        # Number of bases to pad exon/transcript
-        bases = request.form['base_num']
-
-        obj_for_bed = bedmake.RCodeToBedFile(
-            test_code=r_code,
-            include_exon=exon_or_transcript,
-            ref_genome=selected_build,
-            padded=selected_padding,
-            num_bases=bases)
-
-        output_content = obj_for_bed.create_string_bed()
-
-        # file_content_bytes = file_content_string.encode('utf-8')
-
-        try:
-            response = Response(
-                output_content, content_type='text/plain; charset=utf-8')
-            file_name = f'generated_file_{r_code}.bed'
-            response.headers['Content-Disposition'] = f'attachment; filename={file_name}'
-            return response
-        except Exception as e:
-            return f"Error: {str(e)}"
-
-    """
     all blueprints are to be found below this comment
     (blueprints are modules of flask code that can be used to extend the app)
     including
@@ -151,6 +48,122 @@ def create_app(test_config=None):
     - the Auth blueprint (not yet implemented)
     """
     # install the database blueprint into the module
-    app.register_blueprint(database_blueprint.bp, url_prefix='/database')
-    app.register_blueprint(user_auth.bp, url_prefix='/auth')
+    app.register_blueprint(base, url_prefix='/')
+    app.register_blueprint(
+        database_blueprint.blueprint_db, url_prefix='/database')
+
+    database_blueprint.db.init_app(app)
+    # Should create the tables in the database
+    database_blueprint.create_tables(app.app_context(), database_blueprint.db)
+
     return app
+
+
+"""
+all app routes are to be found below this comment
+including 
+- the home page
+- the search page
+- the gene list page
+- the download page
+
+"""
+base = Blueprint('base', __name__, url_prefix='/')
+
+
+@base.route("/")
+def hello_world():
+    """
+    This is the home page route, it renders the home.html template with 
+    links to search an R number and to go to the database or Auth pages 
+    """
+    return render_template("home.html")
+
+
+@base.route("/search", methods=['POST'])
+def test_data():
+    """
+    This is the search page route, it renders the search.html template with
+    the search form and a link to the home page using the Data
+    """
+    if request.method == 'POST':
+        r_code = request.form.get('r')
+        session['r'] = r_code.upper()
+        if r_code:
+            # Create an object instance of the ApiCallsSadie class and fetch the panel
+            api_calls_object = bedmake.RCodeToBedFile(
+                r_code, ref_genome='GRCh38')
+            filtered_api_result = api_calls_object.get_panel_for_genomic_test()
+            panel_name = filtered_api_result['name']
+            panel_version = filtered_api_result['version']
+
+            # Create an object instance of the Parser class and parse the excel file
+            Parsed_results_object = ParserExcel.Parser()
+            filtered_df = Parsed_results_object.parse(r_code=r_code)
+
+            # Create a dictionary to pass to the results.html template for Jinja to render
+            jijna_data = {"df": filtered_df,
+                          "r_json": filtered_api_result, "r": r_code.upper(),
+                          "panel_label": panel_name,
+                          "panel_version": panel_version}
+
+            # if the api call has worked, render the results.html template with Jinja data
+            if filtered_api_result:
+                return render_template("results.html", results=jijna_data)
+            else:
+                return "<p>Panel not found </p><a href='/'>Go back</a>"
+        # Handle cases where 'r' is not provided or other issues
+        return "<p>No data provided or invalid request</p>"
+    # Inform users that a GET request is not supported
+    return "<p>GET request is not supported. Use the search form.</P."
+
+
+@base.route("/search/genelist", methods=['GET'], endpoint="genelist")
+def gene_list():
+    r_code = session.get('r')
+    if r_code is not None:
+        api_calls_object = bedmake.RCodeToBedFile(
+            r_code, ref_genome='GRCh38')
+        panel_info = api_calls_object.get_panel_for_genomic_test()
+        gene_name_panel = api_calls_object.extract_genes_hgnc()
+        panel_name = panel_info['name']
+        panel_version = panel_info['version']
+        data_genepage = {"gene_list": gene_name_panel,
+                         "panel_name": panel_name,
+                         "panel_version": panel_version}
+        return render_template("genelist.html", results=data_genepage)
+    else:
+        return "No 'r' parameter found in the session"
+
+
+@base.route("/search/download", endpoint="download", methods=['POST'])
+def download_file():
+    r_code = session.get('r')
+    # choose between GRCh38 or GRCh37
+    selected_build = request.form['build']
+    # choose between Mane Select transcript coordinates or exon coordinates
+    exon_or_transcript = request.form['version']
+    # add an additonal 50bp padding to the exons or transcript
+    selected_padding = request.form['padding']
+    # Number of bases to pad exon/transcript
+    bases = request.form['base_num']
+
+    obj_for_bed = bedmake.RCodeToBedFile(
+        test_code=r_code,
+        include_exon=exon_or_transcript,
+        ref_genome=selected_build,
+        padded=selected_padding,
+        num_bases=bases)
+
+    output_content = obj_for_bed.create_string_bed()
+
+    # file_content_bytes = file_content_string.encode('utf-8')
+
+    try:
+        response = Response(
+            output_content, content_type='text/plain; charset=utf-8')
+        file_name = f'generated_file_{r_code}.bed'
+        response.headers['Content-Disposition'] = f'attachment; filename={file_name}'
+        return response
+    except Exception as e:
+        return f"Error: {str(e)}"
