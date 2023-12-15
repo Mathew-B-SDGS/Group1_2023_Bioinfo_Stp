e23125967122bc771e373a0ec8bf88a444347df8
ashs <sadie.ash@gosh.nhs.uk>
2023-12-15 11:10:28 +0000

    Change variables names

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from AppBlueprints import database_blueprint
 from modules import ParserExcel, bedmake  # importing local modules ./modules/
 # importing local blueprints ./AppBlueprints/
-from AppBlueprints import database_blueprint, user_auth
+from AppBlueprints import database_blueprint
 
 
 
@@ -102,26 +102,31 @@ def test_data():
         r_code = request.form.get('r')
         session['r'] = r_code.upper()
         if r_code:
-            # Create an object instance of the ApiCallsSadie class and fetch the panel
-            api_calls_object = bedmake.RCodeToBedFile(
+            # Create an object instance of the RCodeToBedFile class 
+            # and fetch the panel info
+            bedmake_object = bedmake.RCodeToBedFile(
                 r_code, ref_genome='GRCh38')
-            filtered_api_result = api_calls_object.get_panel_for_genomic_test()
-            panel_name = filtered_api_result['name']
-            panel_version = filtered_api_result['version']
+            panel_information = bedmake_object.get_panel_for_genomic_test()
+            panel_name = panel_information['name']
+            panel_version = panel_information['version']
 
-            # Create an object instance of the Parser class and parse the excel file
+            # Create an object instance of the Parser class 
+            # and parse the NGTD excel file
             parsed_results_object = ParserExcel.Parser()
-            filtered_df = parsed_results_object.parse(r_code=r_code)
+            filtered_NGTD = parsed_results_object.parse(r_code=r_code)
 
-            # Create a dictionary to pass to the results.html template for Jinja to render
-            jijna_data = {"df": filtered_df,
-                          "r_json": filtered_api_result, "r": r_code.upper(),
+            # Create a dictionary to pass to the results.html template 
+            # for Jinja to render
+            r_results_data = {"df": filtered_NGTD,
+                          "r_json": panel_information,
+                          "r": r_code.upper(),
                           "panel_label": panel_name,
                           "panel_version": panel_version}
 
-            # if the api call has worked, render the results.html template with Jinja data
-            if filtered_api_result:
-                return render_template("results.html", results=jijna_data)
+            # if the api call has worked, render the results.html template 
+            # with results data
+            if panel_information:
+                return render_template("results.html", results=r_results_data)
             else:
                 return "<p>Panel not found </p><a href='/'>Go back</a>"
         # Handle cases where 'r' is not provided or other issues
@@ -134,10 +139,10 @@ def test_data():
 def gene_list():
     r_code = session.get('r')
     if r_code is not None:
-        api_calls_object = bedmake.RCodeToBedFile(
+        bedmake_object = bedmake.RCodeToBedFile(
             r_code, ref_genome='GRCh38')
-        panel_info = api_calls_object.get_panel_for_genomic_test()
-        gene_name_panel = api_calls_object.extract_genes_hgnc()
+        panel_info = bedmake_object.get_panel_for_genomic_test()
+        gene_name_panel = bedmake_object.extract_genes_hgnc()
         panel_name = panel_info['name']
         panel_version = panel_info['version']
         data_genepage = {"gene_list": gene_name_panel,
@@ -169,13 +174,31 @@ def download_file():
 
     output_content = obj_for_bed.create_string_bed()
 
+    bedmake_object = bedmake.RCodeToBedFile(
+        r_code, ref_genome='GRCh38')
+    panel_info = bedmake_object.get_panel_for_genomic_test()
+    panel_name = panel_info['name']
+    panel_name = panel_name.replace(' ', '_')
+    panel_version = panel_info['version']
+
     # file_content_bytes = file_content_string.encode('utf-8')
 
     try:
         response = Response(
             output_content, content_type='text/plain; charset=utf-8')
-        file_name = f'generated_file_{r_code}.bed'
-        response.headers['Content-Disposition'] = f'attachment; filename={file_name}'
+        base_pad = []
+        entity = []
+        if exon_or_transcript == 'True':
+            entity = 'MANE_exons'
+        else:
+            entity = 'MANE_transcript'
+        if selected_padding == 'True':
+            base_pad = f'{bases}_bp_padding'
+        else:
+            base_pad = 'no_padding'
+        name = f'{panel_name}_{panel_version}_{entity}_{base_pad}.bed'
+        attach = f'attachment; filename={name}'
+        response.headers['Content-Disposition'] = attach
         return response
     except Exception as e:
         return f"Error: {str(e)}"
