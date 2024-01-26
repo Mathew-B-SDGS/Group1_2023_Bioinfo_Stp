6d7ed129dfeb3967f4a1a4539b642de33cf02205
ashs <sadie.ash@gosh.nhs.uk>
2024-01-26 11:16:47 +0000

    Adhere to pep8 standards

--- modules/parser_test_directory.py
+++ modules/parser_test_directory.py
@@ -1,10 +1,17 @@
-import pandas as pd 
+import pandas as pd
+
 
 class Parser:
-    def __init__(self,):
-        self.df = pd.read_excel('./resources/Rare-and-inherited-disease-national-genomic-test-directory-version-5.1.xlsx', header=1)
+    "Parses the National Genomic Test Directory for app"
 
-    def parse(self, r_code):
-        matching_rows = self.df[self.df.iloc[:, 0] == r_code.upper()]
-        return matching_rows
 
+def __init__(self,):
+    self.df = pd.read_excel(("./resources/Rare-and-inherited"
+                             "-disease-national-"
+                             "genomic-test-directory-version-5.1.xlsx"),
+                            header=1)
+
+
+def parse(self, r_code):
+    matching_rows = self.df[self.df.iloc[:, 0] == r_code.upper()]
+    return matching_rows
